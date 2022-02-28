34e93dee6da871f5654d047cce810313b8d0508c
dongho YUN <76724739+dhboys@users.noreply.github.com>
2022-02-28 15:30:35 +0900

    commit

--- config/config.js
+++ config/config.js
@@ -6,14 +6,14 @@ module.exports = {
   "development": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "react-nodebird",
+    "database": "react_nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "react-nodebird",
+    "database": "react_nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
